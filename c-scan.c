a7c314cd1d5d439804d90a44126e54b293cdff9d
anjali <anjali.rawat8181@gmail.com>
2026-06-29 17:21:34 +0530

    Add c-scan implementation

---
+++ c-scan.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, head, size, total = 0;
+
+    printf("Enter number of requests: ");
+    scanf("%d", &n);
+
+    int req[n];
+
+    printf("Enter requests: ");
+    for(int i = 0; i < n; i++)
+        scanf("%d", &req[i]);
+
+    printf("Enter head position: ");
+    scanf("%d", &head);
+
+    printf("Enter disk size: ");
+    scanf("%d", &size);
+
+    for(int i = 0; i < n - 1; i++)
+        for(int j = i + 1; j < n; j++)
+            if(req[i] > req[j]) {
+                int temp = req[i];
+                req[i] = req[j];
+                req[j] = temp;
+            }
+
+    int pos = 0;
+
+    while(pos < n && req[pos] < head)
+        pos++;
+
+    printf("\nOrder:\n");
+
+    for(int i = pos; i < n; i++) {
+        total += abs(head - req[i]);
+        head = req[i];
+        printf("%d ", head);
+    }
+
+    total += abs(head - (size - 1));
+    total += size - 1;
+    head = 0;
+
+    for(int i = 0; i < pos; i++) {
+        total += abs(head - req[i]);
+        head = req[i];
+        printf("%d ", head);
+    }
+
+    printf("\n\nTotal Head Movement = %d", total);
+
+    return 0;
+}
